f5dac6595bbd3413c07c5750c26ef79421b78ac5
aaronyuen2 <35553648+aaronyuen2@users.noreply.github.com>
2021-02-05 16:51:10 +0800

    Day 2 Development
    - Physics movement based on movement vector
    - Sphere added to simulate this

---
+++ PlanetaryDefence/Assets/GAME/Scripts/playerControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerControl : MonoBehaviour
+{
+    public JoyconDemo jcReading;
+    float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        speed = jcReading.accel.y * 20f;
+        this.GetComponent<Rigidbody>().AddForce(Vector3.forward * speed * Time.deltaTime);
+        
+    }
+}
